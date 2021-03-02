c34d4e6343b3b0bd0b6db2c18bfa15234db83d9d
Liuming <lfan.canada@gmail.com>
2021-03-02 06:28:40 +0000

    Template Literals for Implement assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual,expected) {
   if (actual === expected) {
-    console.log("✅ " + actual + " === " + expected);
+    console.log(`✅ ${actual} === ${expected}`);
   } else {
-    console.log("❌ " + actual + " !== " + expected);
+    console.log(`❌ ${actual} !== ${expected}`);
   }
 };
 
