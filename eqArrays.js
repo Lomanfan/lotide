48dbe1017b4974ba42880d71ce4e7678410bea94
Liuming <lfan.canada@gmail.com>
2021-03-03 21:56:16 +0000

    Implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ${actual} !== ${expected}`);
+  }
+};
+
+
+//Implement a function eqArrays which takes in two arrays and
+//returns true or false, based on a perfect match.
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true); // => should PASS
