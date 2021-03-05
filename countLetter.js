c0cd91d1d2fc1d4e90acadbbb1d0820e5e68eb18
Liuming <lfan.canada@gmail.com>
2021-03-05 02:03:08 +0000

    Count letters in str and retun object key value pair

---
+++ countLetter.js
@@ -0,0 +1,30 @@
+// const assertEqual = function(actual,expected) {
+//   if (actual === expected) {
+//     console.log(`✅ ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌ ${actual} !== ${expected}`);
+//   }
+// };
+
+const countLetters = function(str) {
+  let letterTotal = {}; // {} object
+  let strArray = str.split(''); //''split after each letter
+  //str2 = "123,240,432"
+  //str2.split(",")>> [123,240,432]
+  //console.log(strArray);
+
+  for (let i = 0; i < strArray.length; i++) {
+    if (strArray[i] !== " ") {
+      if (letterTotal[strArray[i]]) {
+        letterTotal[strArray[i]] += 1;
+      } else {
+        letterTotal[strArray[i]] = 1;
+      }
+    }
+
+  }
+  return letterTotal; //return key value pair object;
+};
+
+console.log(countLetters('LHL'));
+console.log(countLetters('Lighthouse is Lit'));
