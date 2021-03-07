da4be5cc6e357af8463026c38577457a2fca083b
Liuming <lfan.canada@gmail.com>
2021-03-07 08:00:47 +0000

    Keep pushing until callback is true and return value

---
+++ takeUntil.js
@@ -0,0 +1,55 @@
+//The function will return a "slice of the array with elements taken from the beginning." 
+//It should keep going until the callback/predicate returns a truthy value.
+//Implement takeUntil which will keep collecting items from a provided array 
+//until the callback provided returns a truthy value.
+
+
+const takeUntil = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return results;
+    } else {
+      results.push(item);
+    }
+  }
+  return results;
+};
+
+// //Expected Input
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+//Expected Output
+// [ 1, 2, 5, 7, 2 ]
+// ---
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+console.log('---------------------------------------');
+
+// Test with assertArraysEquals
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(newArr1, newArr2) {
+  if (eqArrays(newArr1, newArr2)) {
+    console.log(`✅ ${newArr1} === ${newArr2}`);
+  } else {
+    console.log(`❌ ${newArr1} !== ${newArr2}`);
+  }
+};
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
+
