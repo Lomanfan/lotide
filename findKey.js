d00219528f46f4e2ab76abb638a177a0cb76dff9
Liuming <lfan.canada@gmail.com>
2021-03-08 04:56:04 +0000

    findKey.js completed.

--- findKey.js
+++ findKey.js
@@ -5,20 +5,15 @@
 
 const findKey = function(object, callback) {
   for (const key in object) {
-    if (callback(object[key])) {                ///findKey.object[key] is true
-      return key;                                                 stars: 2
-    }
+    // console.log("key", key);
+    // console.log("object[key]", object[key]);
+    if (callback(object[key])) {                //passing in object with value of stars: 1
+      return key;                                               
+    } 
   }
+  // console.log("result",result);
+  return undefined;
 };
-
-findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri": { stars: 3 },
-  "noma": { stars: 2 },                                 
-  "elBulli": { stars: 3 },
-  "Ora": { stars: 2 },
-  "Akelarre": { stars: 3 }
-}, x => x.stars === 2);
 
 console.log(findKey({
   "Blue Hill": { stars: 1 },
@@ -27,13 +22,22 @@ console.log(findKey({
   "elBulli": { stars: 3 },
   "Ora": { stars: 2 },
   "Akelarre": { stars: 3 }
-}, x => x.stars === 2)); // => "noma"
+}, x => x.stars === 2)); // => "noma"       // parm x is star2, star2.stars =2, does it ===2? 
 
 
-// const assertEqual = function(actual,expected) {
-//   if (actual === expected) {
-//     console.log(`✅ ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌ ${actual} !== ${expected}`);
-//   }
-// };
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual((findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2)), 'noma');
