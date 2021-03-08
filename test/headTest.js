d2e5c8e0f380d2bc6558761e507d9b8f9f0cef3d
Liuming <lfan.canada@gmail.com>
2021-03-08 22:58:35 +0000

    Mocha-chai Tested, headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,13 +1,25 @@
 
 // test/head.js & assertEqualTest.js
-
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');  
+  });
+    
+});
+
 
 // TEST CODE
+// const assertEqual = require('../assertEqual');
 // ...
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([6]), 5);
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), 5);
+// assertEqual(head([6]), 5);
