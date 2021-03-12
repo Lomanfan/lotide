4280261f29f1e6f0cee939276a89b61536d737db
Lomanfan <77166991+Lomanfan@users.noreply.github.com>
2021-03-12 09:17:04 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,13 +1,7 @@
 
 const map = function(array,callback) {
-    // temporary code:
-    //console.log('array: ', array);
-    //console.log('callback: ', callback);
   const results = [];
   for (let item of array) {
-    // console.log('item BEFORE: ', item);
-    // console.log('item AFTER: ', callback(item));
-    // console.log('---');                  //remove debug friendly console.log
     results.push(callback(item));
   }
   return results;
@@ -18,9 +12,6 @@ const word2 = ["school", "control", "to", "play", "jerry"];
 
 const results1 = map(words, word => word[0]);
 const results2 = map(word2, word => word[0]);
-// console.log(results1);
-// console.log(results2);
-
 
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
