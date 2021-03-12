592e61c2d6f4ab0dff236d169a8d99689edeb0ba
Lomanfan <77166991+Lomanfan@users.noreply.github.com>
2021-03-12 09:15:48 -0700

    Update findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,33 +1,12 @@
 
-//Task:
-//It should scan the object and return the first key which contains the given value.
-//If no key with that given value is found, then it should return undefined.
-
-
-
-const findKeyByValue = function(bestTVShowsByGenre, showName) { //bestTVShowByGenre {}, showName ""
-// let byGenre = {};                                 //Object {}
-  let tvKey = Object.keys(bestTVShowsByGenre);  //Object.keys() returns an array of KEYS
-  // console.log(tvKey);
+const findKeyByValue = function(bestTVShowsByGenre, showName) { 
+  let tvKey = Object.keys(bestTVShowsByGenre);  
   for (const key of tvKey) {
     if (bestTVShowsByGenre[key] === showName) {
       return key;
     }
   }
 };
-  // for (let i = 0; i < tvKey.length; i++) {
-  //   let name = bestTVShowsByGenre[tvKey[i]]
-  //   console.log(name);   //Why only one name return??????
-  //   if (name === showName) {
-  //     byGenre = bestTVShowsByGenre[tvKey[i]];
-  //   return byGenre;
-  //   console.log(byGenre);
-  //   } else {
-  //     undefined;
-  //   }
-  // }
-  // return byGenre;
-  // };
 
 //Test Assertions:
 const bestTVShowsByGenre = {
@@ -35,9 +14,6 @@ const bestTVShowsByGenre = {
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire"
 };
-
-// console.log(findKeyByValue(bestTVShowsByGenre,"The Wire"));
-
 
 const assertEqual = function(actual,expected) {
   if (actual === expected) {
