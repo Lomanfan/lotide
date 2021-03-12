70908ba11796cd6880426fe10f823254c6714eae
Lomanfan <77166991+Lomanfan@users.noreply.github.com>
2021-03-12 09:13:05 -0700

    Update middle.js

--- middle.js
+++ middle.js
@@ -1,20 +1,3 @@
-//Challenge//Implement middle which will take in an array and
-//return the middle-most element(s) of the given array.
-//The middle function should return an array with only the middle element(s) of
-//the provided array.
-//This means that the length of the returned elements could vary.
-
-// For arrays with one or two elements, there is no middle. Return an empty array.
-// middle([1]) // => []
-// middle([1, 2]) // => []
-// For arrays with odd number of elements, an array containing a single middle element should be returned.
-// middle([1, 2, 3]) // => [2]
-// middle([1, 2, 3, 4, 5]) // => [3]
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-// middle([1, 2, 3, 4]) // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-
-
 
 //find Middle
 const middle = function(array) {
@@ -31,19 +14,3 @@ const middle = function(array) {
 };
 
 module.exports = middle;
-
-//arrayY= [5, 2, 6, 4, 9, 12, 18, 13, 1, 17, 25, 33, 55, 62,66];
-//console.log(arrayY.length);
-//console.log(middle(arrayY));
-
-// console.log(middle([1]));
-// console.log(middle([1, 2, 3]));
-// console.log(middle([1, 2, 3, 4]));
-// console.log(middle([1, 2, 3, 4, 5, 6]));
-
-// console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-
-// console.log(assertArraysEqual(middle([1, 2, 3]), [1, 2]));
-// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [2, 3]));
-
-// console.log(eqArrays([1,2],[1,2]));
