0edbe68fc1759511cb25908badd7a6b125218ced
Lomanfan <77166991+Lomanfan@users.noreply.github.com>
2021-03-12 09:10:10 -0700

    Update findKey.js

--- findKey.js
+++ findKey.js
@@ -1,17 +1,10 @@
-//Function scans the object and return the FIRST key for which
-//the callback returns a truthy value. NO key is found => undefined.
-
-//Question: set undefined!!! Ask mentor
 
 const findKey = function(object, callback) {
   for (const key in object) {
-    // console.log("key", key);
-    // console.log("object[key]", object[key]);
-    if (callback(object[key])) {                //passing in object with value of stars: 1
+    if (callback(object[key])) {         
       return key;                                               
     } 
   }
-  // console.log("result",result);
   return undefined;
 };
 
@@ -22,8 +15,7 @@ console.log(findKey({
   "elBulli": { stars: 3 },
   "Ora": { stars: 2 },
   "Akelarre": { stars: 3 }
-}, x => x.stars === 2)); // => "noma"       // parm x is star2, star2.stars =2, does it ===2? 
-
+}, x => x.stars === 2)); 
 
 const assertEqual = function(actual,expected) {
   if (actual === expected) {
