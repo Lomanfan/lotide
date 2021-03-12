df7ffefd7610ef9dde14f27e77552b1232a2e607
Lomanfan <77166991+Lomanfan@users.noreply.github.com>
2021-03-12 09:12:02 -0700

    Update takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,8 +1,3 @@
-//The function will return a "slice of the array with elements taken from the beginning." 
-//It should keep going until the callback/predicate returns a truthy value.
-//Implement takeUntil which will keep collecting items from a provided array 
-//until the callback provided returns a truthy value.
-
 
 const takeUntil = function (array, callback) {
   const results = [];
@@ -16,21 +11,6 @@ const takeUntil = function (array, callback) {
   return results;
 };
 
-// //Expected Input
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-
-//Expected Output
-// [ 1, 2, 5, 7, 2 ]
-// ---
-// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
-console.log('---------------------------------------');
 
 // Test with assertArraysEquals
 const eqArrays = function(arr1, arr2) {
