ac47b8efc5e1fdbd0c46d1d1b97862df04e58211
Liuming <lfan.canada@gmail.com>
2021-04-01 20:54:23 +0000

    tail function returns items of array except for the first item; updated assertEqual function on test codes

--- assertEqual.js
+++ assertEqual.js
@@ -13,8 +13,8 @@ const assertEqual = function (actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
 
 module.exports = assertEqual;
--- tail.js
+++ tail.js
@@ -1,14 +1,23 @@
+//Create a function tail which returns the "tail" of an array: everything except for the first item (head) of the provided array.
 
-const tail = function(y){
-  return y.slice(1);
-}
+const assertEqual = require('./assertEqual');
+
+const tail = function (array) {
+  if (array) {
+    return array.slice(1)
+  }
+};
+
+
+//TEST CODE:
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail! JavaScript doesn't allow the use of === or == to compare two arrays.
+
+// TEST CODE TO ENSURE ORIGINAL ARRAY IS NOT MODIFIED FROM TAIL FUNCTION:
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
 
 module.exports = tail;
 
-// // Test Case: Check the original array
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// const tailArray = tail(words);
-
-// assertEqual(words.length, 3); 
-// assertEqual(tailArray[0], "Lighthouse"); 
-// assertEqual(tailArray[1], "Labs"); 
