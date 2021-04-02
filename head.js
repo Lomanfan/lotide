cdc40283476c92a7fb86e0b053a3cfb189ed1edb
Liuming <lfan.canada@gmail.com>
2021-04-02 23:34:27 +0000

    refactored, removed return undefine step

--- head.js
+++ head.js
@@ -3,11 +3,7 @@
 const assertEqual = require('./assertEqual');
 
 const head = function (array) {
-  if (!array.length) {
-    return undefined;
-  } else {
-    return array[0];
-  }
+  return array[0];
 };
 
 assertEqual(head([5, 6, 7]), 5);
