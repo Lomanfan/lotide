8fbd69acbd54a210c26c2bf9392c54eaeb5d5d4e
Liuming <lfan.canada@gmail.com>
2021-04-03 23:13:08 +0000

    find obj key if cb is truthy

--- findKey.js
+++ findKey.js
@@ -1,20 +1,16 @@
 //Function scans the object and return the FIRST key for which
-//the callback returns a truthy value. NO key is found => undefined.
+//the callback returns a truthy value.
 
-//Question: set undefined!!! Ask mentor
-
-const findKey = function(object, callback) {
+const findKey = function (object, callback) {
   for (const key in object) {
-    // console.log("key", key);
-    // console.log("object[key]", object[key]);
-    if (callback(object[key])) {                //passing in object with value of stars: 1
-      return key;                                               
-    } 
+    if (callback(object[key])) {
+      return key;
+    }
   }
-  // console.log("result",result);
   return undefined;
 };
 
+//TEST CODE:
 console.log(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri": { stars: 3 },
@@ -22,10 +18,9 @@ console.log(findKey({
   "elBulli": { stars: 3 },
   "Ora": { stars: 2 },
   "Akelarre": { stars: 3 }
-}, x => x.stars === 2)); // => "noma"       // parm x is star2, star2.stars =2, does it ===2? 
+}, x => x.stars === 2)); // => "noma"
 
-
-const assertEqual = function(actual,expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅ ${actual} === ${expected}`);
   } else {
