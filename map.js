a5b88d38b30437372a16fbe303eb97601b18f028
Liuming <lfan.canada@gmail.com>
2021-04-03 21:02:41 +0000

    recreate a map function instead of using built-in map

--- map.js
+++ map.js
@@ -1,28 +1,22 @@
 
-const map = function(array,callback) {
-    // temporary code:
-    //console.log('array: ', array);
-    //console.log('callback: ', callback);
-  const results = [];
-  for (let item of array) {
-    // console.log('item BEFORE: ', item);
-    // console.log('item AFTER: ', callback(item));
-    // console.log('---');                  //remove debug friendly console.log
-    results.push(callback(item));
+const map = function(array, callback) {
+
+  let results = [];
+  for (let arr of array) {
+    results.push(callback(arr));
   }
   return results;
 };
 
-const words = ["ground", "control", "to", "major", "tom"];
+
+const word1 = ["ground", "control", "to", "major", "tom"];
 const word2 = ["school", "control", "to", "play", "jerry"];
 
-const results1 = map(words, word => word[0]);
+const results1 = map(word1, word => word[0]);
 const results2 = map(word2, word => word[0]);
-// console.log(results1);
-// console.log(results2);
+// const result1 = map(word1, word = word => word[0]);
 
-
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -32,14 +26,14 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const assertArraysEqual = function(newArr1, newArr2) {
-  if (eqArrays(newArr1,newArr2)) {
+  if (eqArrays(newArr1, newArr2)) {
     console.log(`✅ ${newArr1} === ${newArr2}`);
   } else {
     console.log(`❌ ${newArr1} !== ${newArr2}`);
   }
 };
 
-assertArraysEqual(results1,results2);
+assertArraysEqual(results1, results2);
 
 
 module.exports = map;
