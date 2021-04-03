983fa492bd35bc6cbc2fa416c20be058116d8b59
Liuming <lfan.canada@gmail.com>
2021-04-03 04:37:04 +0000

    assert two objs and console.log msg; with test code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,38 +1,19 @@
 
+const eqObjects = require('./eqObjects');
 
-//Task: Implement assertObjectsEqual which will take in two objects and console.log an appropriate message to the console.
-
-
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-  for (const key in object1) {
-    // console.log(object1[key]);
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertObjectsEqual = function(actual, expected) {
+const assertObjectsEqual = function (actual, expected) {
   const inspect = require('util').inspect;
-  if (eqObjects(actual,expected)) {
+  if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ ${inspect(actual)} === ${inspect(expected)}`);
   } else {
     console.log(`❌❌❌ ${inspect(actual)} !== ${inspect(expected)}`);
   }
-};                         
+};
 
+//TEST CODE:
 ab = { a: "1", b: "2" };
 ba = { b: "2", a: "1" };
-console.log(assertObjectsEqual(ab,ba));
-
-
+console.log(assertObjectsEqual(ab, ba));
 
 
 module.exports = assertObjectsEqual;
-
-//Expected Output in console:
-//✅✅✅ Assertion Passed: { a: '1', b: 2 } === { b: 2, a: '1' }
