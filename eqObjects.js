fb25adc35446c734ed9b4e663544a9d9213b55ff
Liuming <lfan.canada@gmail.com>
2021-04-03 04:19:42 +0000

    Obj comparison,Array.isArray, with test code

--- eqObjects.js
+++ eqObjects.js
@@ -1,41 +1,34 @@
 //For Objects Comparison
 // Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 
-
-//pseudo codes: 
-// if each objt1 key !== objt2 key stop, output false;
-// if objt1[key] (value)=== objt2[key] (value), out out true.
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-    return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function (object1, object2) {
+
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
-  for (const key in object1) {
-    // console.log(object1[key]);
-    if (object1[key] !== object2[key]) {
-     return false;
+
+  for (let key in object1) {
+    if (Array.isArray(object1[key] && Array.isArray(object2[key]))) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+        }
+      }
     }
   }
   return true;
 };
 
+
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-  console.log(`✅ ${actual} === ${expected}`);
+    console.log(`✅ ${actual} === ${expected}`);
   } else {
-  console.log(`❌ ${actual} !== ${expected}`);
+    console.log(`❌ ${actual} !== ${expected}`);
   }
 };
 
@@ -45,11 +38,16 @@ const abc = { a: "1", b: "2", c: "3" };
 
 console.log(eqObjects(ab, ba));
 console.log(eqObjects(ab, abc));
-
 assertEqual(eqObjects(ab, ba), true); // => true
+assertEqual(eqObjects(ab, abc), false); // => false
 
-eqObjects(ab, abc); 
-// => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
 
 
 module.exports = eqObjects;
