cd48f65648ade92168bb0574fd7962bdde6da20e
Liuming <lfan.canada@gmail.com>
2021-04-03 04:33:42 +0000

    Obj comparison,Array.isArray, with test code

--- eqObjects.js
+++ eqObjects.js
@@ -23,7 +23,7 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
-
+//TEST CODE:
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅ ${actual} === ${expected}`);
@@ -32,22 +32,22 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const abc = { a: "1", b: "2", c: "3" };
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// const abc = { a: "1", b: "2", c: "3" };
 
-console.log(eqObjects(ab, ba));
-console.log(eqObjects(ab, abc));
-assertEqual(eqObjects(ab, ba), true); // => true
-assertEqual(eqObjects(ab, abc), false); // => false
+// console.log(eqObjects(ab, ba));
+// console.log(eqObjects(ab, abc));
+// assertEqual(eqObjects(ab, ba), true); // => true
+// assertEqual(eqObjects(ab, abc), false); // => false
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false); // => false
 
 
 module.exports = eqObjects;
