38262405d3eacc6c9b78c420f08e99397b907dd6
Liuming <lfan.canada@gmail.com>
2021-04-05 22:21:37 +0000

    refactored head function & chai testing

--- head.js
+++ head.js
@@ -6,8 +6,8 @@ const head = function (array) {
   return array[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
 
 module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,33 +1,24 @@
 
-// test/head.js & assertEqualTest.js
 const assert = require('chai').assert;
 const head = require('../head');
 
 
-describe("test for head function", () => {
+describe("#head function test", () => {
+  
   it("returns 5 for [5, 6, 7]", () => {
-    assert.strictEqual(head([5, 6, 7]), 5);
+    assert.deepEqual(head([5, 6, 7]), 5);
   });
 
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+    assert.deepEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   });
 
   it("returns 'undefine' for empty array", () => {
-    assert.strictEqual(head([]), undefined);
+    assert.deepEqual(head([]), undefined);
   });
 
   it("returns 'undefine' for empty array", () => {
-    assert.strictEqual(head([]), undefined);
+    assert.deepEqual(head([]), undefined);
   });
 
 });
-
-
-// TEST CODE
-// const assertEqual = require('../assertEqual');
-// ...
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([]), 5);
-// assertEqual(head([6]), 5);
