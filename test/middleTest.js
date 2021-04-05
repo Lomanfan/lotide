d63cccbd365c7433f19b16cb01dcdcf2616d7ead
Liuming <lfan.canada@gmail.com>
2021-04-05 23:23:50 +0000

    refactored middle function & chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,20 @@
+
 const assert = require('chai').assert;
 const middle = require('../middle');
-// const eqArrays = require('../eqArrays');
-// const assertArraysEqual = require('../assertArraysEqual');
 
-// TEST CODE
-// ...
-describe("#middle", () => {
+
+describe("#middle function test", () => {
+  
   it("returns [2] for [1, 2, 3]", () => {
-    assert.deepEqual(middle([1, 2, 3]),[2]);
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
 });
